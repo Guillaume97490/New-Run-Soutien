25a6861b01c9bf88ebc46d7e126ab7e7aca0d79a
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-06-24 15:52:10 +0400

    random for seed pivot

--- database/seeds/CourUserTableSeeder.php
+++ database/seeds/CourUserTableSeeder.php
@@ -14,11 +14,33 @@ class CourUserTableSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('cour_user')->insert(array(
-            array('user_id' => '4', 'cour_id' => '1', 'date' => '2019/06/25', 'start_time' => '08:00:00', 'end_time' => '09:00:00'),
-            array('user_id' => '6', 'cour_id' => '1', 'date' => '2019/06/25', 'start_time' => '08:00:00', 'end_time' => '09:00:00'),
+        //  DB::table('cour_user')->insert(array(
+        //     array('user_id' => '4', 'cour_id' => '1', 'date' => '2019/06/25', 'start_time' => '08:00:00', 'end_time' => '09:00:00'),
+        //     array('user_id' => '6', 'cour_id' => '1', 'date' => '2019/06/25', 'start_time' => '08:00:00', 'end_time' => '09:00:00'),
 
 
-        ));
+        // ));
+        $nb_product = 10;
+
+    $i = 0;
+
+    while ($i < $nb_product) :
+
+        $liste = [
+            [
+            'user_id' => rand(6,20),
+            'cour_id' => rand(1,6),
+            'date' => '2019/06/25',
+            'start_time' => '08:00:00',
+            'end_time' => '09:00:00',
+
+            ]
+        ];
+
+        DB::table('cour_user')->insert($liste);
+
+        $i++;
+
+    endwhile;
     }
 }
